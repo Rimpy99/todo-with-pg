e59cd14187d5a3428703391437a9a83fa1c1d4de
Paweł Wrona <85957282+Rimpy99@users.noreply.github.com>
2023-08-06 14:56:41 +0200

    [CLIENT] React set up, materialui added

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -7,7 +7,6 @@ const router = express.Router();
 
 router.get('/todos', getTodos);
 router.post('/add', addTodo);
-// router.put('/:id/change', changeTodo);
 router.delete('/:id/delete', deleteTodo);
 
 export default router;
